f1791eeb3cc7492ea6a7e929f7cc70fdbfb35670
Vladislav <serov.vladsilaw@yandex.ru>
2022-01-17 20:36:17 +0300

    Added a collection creation form

--- src/Controller/CreateCollectionController.php
+++ src/Controller/CreateCollectionController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\ItemCollection;
+use App\Form\CreateCollectionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateCollectionController extends AbstractController
 {
-    #[Route('/create/collection', name: 'create_collection')]
-    public function index(): Response
+    #[Route('/create', name: 'app_create_collection')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $itemCollection = new ItemCollection();
+
+        $form = $this->createForm(CreateCollectionType::class, $itemCollection);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+
+            $entityManager->persist($itemCollection);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_main');
+        }
+
         return $this->render('create_collection/index.html.twig', [
-            'controller_name' => 'CreateCollectionController',
+            'createCollectionForm' => $form->createView(),
+
         ]);
     }
 }
